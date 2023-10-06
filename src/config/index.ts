300e030cbf7cb365c773285e2233b63295dbc9fd
pabel1 <pabelahamed39@gmail.com>
2023-10-06 23:19:42 +0600

    axios and some service url setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,35 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-import path from 'path'
+import path from 'path';
+import { z } from 'zod';
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
+
+const envVarsZodSchema = z.object({
+  NODE_ENV: z.string(),
+  PORT: z
+      .string()
+      .default('3030')
+      .refine((val) => Number(val)),
+  // JWT_SECRET: z.string(),
+  REDIS_URL: z.string(),
+  AUTH_SERVICE_URL: z.string(),
+  CORE_SERVICE_URL: z.string(),
+  // CLOUDINARY_CLOUD_NAME: z.string(),
+  // CLOUDINARY_API_KEY: z.string(),
+  // CLOUDINARY_API_SECRET: z.string()
+});
+
+const envVars = envVarsZodSchema.parse(process.env);
+
 export default {
-  port: process.env.PORT,
-  Database_URL: process.env.DB_URL,
-  defaultPass: process.env.DEFAULT_PASS,
-  env: process.env.NODE_ENV,
+  port: envVars.PORT,
+  // Database_URL: envVars.DB_URL,
+  // defaultPass: envVars.DEFAULT_PASS,
+  env: envVars.NODE_ENV,
   redis: {
-    url: process.env.REDIS_URL
+    url: envVars.REDIS_URL
 },
-// authServiceUrl: envVars.AUTH_SERVICE_URL,
-// coreServiceUrl: envVars.CORE_SERVICE_URL,
+authServiceUrl: envVars.AUTH_SERVICE_URL,
+coreServiceUrl: envVars.CORE_SERVICE_URL,
 }
